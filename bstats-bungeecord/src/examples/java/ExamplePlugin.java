fe1c8de342a48c381fed75c691afad75b59b0eb9
Bastian Oppermann <bastianoppermann1997@gmail.com>
2020-01-13 14:33:20 +0100

    Remove unused import in example plugin for Bungeecord

--- bstats-bungeecord/src/examples/java/ExamplePlugin.java
+++ bstats-bungeecord/src/examples/java/ExamplePlugin.java
@@ -1,8 +1,6 @@
 import net.md_5.bungee.api.plugin.Plugin;
 import org.bstats.bungeecord.Metrics;
 
-import java.util.concurrent.Callable;
-
 public class ExamplePlugin extends Plugin {
 
     @Override
