cebbdbde3cae2af7c03fa62ac13f1cdb256375bc
Ben <benbergen12@gmail.com>
2023-01-14 19:12:48 -0500

    Submit data asynchronously

--- bukkit/src/main/java/org/bstats/bukkit/Metrics.java
+++ bukkit/src/main/java/org/bstats/bukkit/Metrics.java
@@ -70,7 +70,7 @@ public class Metrics {
                 enabled,
                 this::appendPlatformData,
                 this::appendServiceData,
-                submitDataTask -> Bukkit.getScheduler().runTask(plugin, submitDataTask),
+                submitDataTask -> Bukkit.getScheduler().runTaskAsynchronously(plugin, submitDataTask),
                 plugin::isEnabled,
                 (message, error) -> this.plugin.getLogger().log(Level.WARNING, message, error),
                 (message) -> this.plugin.getLogger().log(Level.INFO, message),
