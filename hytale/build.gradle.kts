209af6141082da4130f376797f12fc6a303fa0b7
xXSchrandXx <marvinschrand@yahoo.de>
2026-02-15 19:00:01 +0100

    Add hytale

---
+++ hytale/build.gradle.kts
@@ -0,0 +1,29 @@
+plugins {
+    java
+}
+
+repositories {
+    mavenCentral()
+    maven {
+        name = "hytale"
+        url = uri("https://maven.hytale.com/release")
+    }
+}
+
+dependencies {
+    compileOnly("com.hypixel.hytale:Server:2026.01.24-6e2d4fc36")
+    api(project(":base")) {
+        isTransitive = true
+    }
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+    withJavadocJar()
+    withSourcesJar()
+}
